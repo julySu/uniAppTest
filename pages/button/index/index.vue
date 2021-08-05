9956cd84f7e8f391edf8386f71ff3996b0c53c49
sumei <707452448@qq.com>
2021-08-05 17:30:18 +0800

    no message

---
+++ pages/button/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		<button type="default" size="mini" :disabled="false" hover-start-time="20">button</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
